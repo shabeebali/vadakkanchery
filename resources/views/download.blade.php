fdfe65f39461c7bf9142146ab141b9283190ade2
Shabeeb <shabeeboali@gmail.com>
2021-11-14 17:38:47 +0530

    vfedsljk

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -6,6 +6,9 @@ table, th, td {
   border: 1px solid black;
   border-collapse: collapse;
 }
+td {
+    padding: 10px;
+}
 .fw {
     width: 100%;
 }
@@ -26,26 +29,54 @@ table, th, td {
                 <td colspan="2" class="center py">വാടിയിൽ വടക്കാഞ്ചേരി കുടുംബ സമിതി</td>
             </tr>
             <tr>
-                <td class="p">
+                <td>
                     Member Details
                     <br/>
                     Name: {{ \Illuminate\Support\Str::headline($member->name) }}
                     <br/>
                     Address: {{ \Illuminate\Support\Str::headline($member->address) }}
                     <br/>
-                    DOB: {{ \Illuminate\Support\Str::headline($member->dob) }}
+                    DOB: {{ $member->dob }}
                 </td>
-                <td class="p">
+                <td>
                     Spouse Details
                     <br/>
                     Name: {{ \Illuminate\Support\Str::headline($member->spouse->name) }}
                     <br/>
                     Address: {{ \Illuminate\Support\Str::headline($member->spouse->address) }}
                     <br/>
-                    DOB: {{ \Illuminate\Support\Str::headline($member->spouse->dob) }}
+                    DOB: {{ $member->spouse->dob }}
                 </td>
             </tr>
-        </tbody
-    </table
+        </tbody>
+    </table>
+    <table class="table">
+        <tbody class="table">
+            <tr>
+                <td>Name</td>
+                <td>DOB</td>
+                <td>Blood Group</td>
+            </tr>
+            @foreach($member->children as $child)
+                <tr>
+                    <td>{{ \Illuminate\Support\Str::headline($child->name)}}</td>
+                    <td>{{$child->dob}}</td>
+                    <td>{{$child->blood_group}}</td>
+                <tr>
+                @if ($child->married)
+                    <tr>
+                        <td>{{ \Illuminate\Support\Str::headline($child->spouse->name)}}</td>
+                        <td>{{$child->spouse->dob}}</td>
+                        <td>{{$child->spouse->blood_group}}</td>
+                    <tr>
+                    @foreach ($child->children as $grandChild)
+                        <tr>
+                            <td>{{ \Illuminate\Support\Str::headline($grandChild->name)}}</td>
+                            <td>{{$grandChild->dob}}</td>
+                            <td>{{$grandChild->blood_group}}</td>
+                        <tr>
+                    @endforeach
+                @endif
+            @endforeach
     <div class="page-break"></div>
 @endforeach
