d2b58cbba1941d4a70b77ff3ab010de6e29aa8f7
Shabeeb <shabeeboali@gmail.com>
2021-11-14 17:29:33 +0530

    dsavmh

--- resources/views/download.blade.php
+++ resources/views/download.blade.php
@@ -20,7 +20,27 @@ table, th, td {
     <table class="table fw">
         <tbody class="table">
             <tr>
-                <td class="center py">{{$member->name}} - {{$member->spouse->name}}</td>
+                <td colspan="2" class="center py">വാടിയിൽ വടക്കാഞ്ചേരി കുടുംബ സമിതി</td>
+            </tr>
+            <tr>
+                <td>
+                    Member Details
+                    <br/>
+                    {{ \Illuminate\Support\Str::headline($member->name) }}
+                    <br/>
+                    {{ \Illuminate\Support\Str::headline($member->address) }}
+                    <br/>
+                    {{ \Illuminate\Support\Str::headline($member->dob) }}
+                </td>
+                <td>
+                    Spouse Details
+                    <br/>
+                    {{ \Illuminate\Support\Str::headline($member->spouse->name) }}
+                    <br/>
+                    {{ \Illuminate\Support\Str::headline($member->spouse->address) }}
+                    <br/>
+                    {{ \Illuminate\Support\Str::headline($member->spouse->dob) }}
+                </td>
             </tr>
         </tbody
     </table
